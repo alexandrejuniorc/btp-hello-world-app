5bca5671cd6071aa780f1b811a8081904217bb8c
Alexandre Junior <alexandre.junior@lab2dev.com>
2024-01-05 14:52:24 +0000

    feat: create a button with function to active an alert with javascript

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,15 +1,20 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.m.MessageBox} MessageBox
+     * @param {typeof sap.m.MessageToast} MessageToast
      */
-    function (Controller) {
+    function (Controller, MessageBox, MessageToast) {
         "use strict";
 
         return Controller.extend("sap.btp.helloworldui5.controller.View1", {
-            onInit: function () {
-
+            onInit: function () {},
+            onPress: function () {
+                MessageBox.alert("You have been alerted!");
             }
         });
     });
